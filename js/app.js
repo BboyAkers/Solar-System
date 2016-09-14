588930dd3544aa1817d3f85c1c99b401b6064863
Austin Akers <austin.akers5@gmail.com>
2016-09-14 01:56:15 -0500

    Added workable scene with sphere

--- js/app.js
+++ js/app.js
@@ -6,8 +6,23 @@ BjsApp.init = function () {
 
     var canvas = document.getElementById('renderCanvas');
 
-    var engine = new BABYLON.Engine(canvas, true)
+    //Create engine object
+    var engine = new BABYLON.Engine(canvas, true);
 
-
+    //Create Scene
     var scene = new BABYLON.Scene(engine);
+
+    //Create Camera
+    var camera = new BABYLON.FreeCamera('FreeCamera', new BABYLON.Vector3(0, 2, -15), scene);
+
+    //Light environment
+    var light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0, 1, 0), scene);
+
+    //Create a sphere
+    var sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, scene);
+    sphere.position.y = 1;
+    //render scene
+    engine.runRenderLoop(function () {
+        scene.render();
+    });
 };
