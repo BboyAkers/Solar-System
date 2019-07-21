b50e21a43335d18a9c4d104eabb3ad52ad7be746
Austin Akers <austin.akers5@gmail.com>
2019-07-21 08:47:30 -0500

    removed unneccessary psudeo code

--- js/app.js
+++ js/app.js
@@ -15,8 +15,6 @@ window.addEventListener('DOMContentLoaded', () => {
     //const the user move the camera
     camera.attachControl(canvas);
 
-    //camera.upperRadiusLimit = 75;
-
     //light
     const light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), scene);
     light.intensity = 0.5;
